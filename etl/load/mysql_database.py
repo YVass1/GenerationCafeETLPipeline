d120c1a00267b2e3e3fa7a025a0193544bd309b4
Yogini Vassaramo <yogini.vassaramo@gmail.com>
2020-10-22 23:59:54 +0200

    Programmatically create database

--- etl/load/mysql_database.py
+++ etl/load/mysql_database.py
@@ -1,20 +1,56 @@
 import etl.load.mysql_database_connection as db_connect
 import re
+
+#CONSTANTS NEEDED
 sql_code_filepath = './etl/load/database_sql_code.txt'
 
-def create_database_tables(filepath):
-    sql_db = db_connect.SQL_database()
-    connection = sql_db.make_connection()
+def create_database(database_name):
+    mysql_server = db_connect.MySQL_Server()
+    connection = mysql_server.make_connection()
     try:
-        with open(filepath, "r") as file_data:
-            sql_code = "".join(file_data.readlines())
-            operation = re.sub(r"[\n\t]*", "", sql_code)
         with connection.cursor() as cursor:
-            cursor.execute(operation)
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS `{database_name}`;")
+            connection.commit()
     except Exception as e:
         print(f"Exception Error: {e}")
     finally:
         connection.close()
 
+def create_database_tables(filepath, database_name):
+    mysql_db = db_connect.MYSQL_database(database_name)
+    connection = mysql_db.make_connection()
+    try:
+        with open(filepath, "r") as file_data:
+            sql_code = "".join(file_data.readlines())
+            commands_string = re.sub(r"[\n\t]*", "", sql_code)
+        with connection.cursor() as cursor:
+            cursor.execute(commands_string)
+    except Exception as e:
+        print(f"Exception Error: {e}")
+    finally:
+        connection.close()
+
+data = {"datetime": ["11/10/2020 08:11"], "location": ["Aberdeen"], "fname": ["John"], "lname": ["Doe"],
+ "purchase": [dict], "total_price": [4.25], "payment_method": ["CARD"], "card_number": ["************1234"]}
+
+sub_data =  {"drink_size": ["large", "medium"], "drink_type": ["tea", "coffee"], "drink_flavour": ["peppermint", "black"],
+ "drink_price": [2.5, 1.75]}
+
+def insert_data_into_tables(data):
+    connection = mysql_db.make_connection()
+    try:
+        with connection.cursor() as cursor:
+            for col in data.items():
+                if col == "purchase":
+                    continue
+                else:
+                    cursor.executemany(operation)
+    except Exception as e:
+        #connection.rollback()
+        print(f"Exception Error: {e}")
+    finally:
+        connection.close()
+
 if __name__ == "__main__":
-    create_database_tables(sql_code_filepath)
+    create_database("final_project_team1")
+    create_database_tables(sql_code_filepath, "final_project_team1")
--- etl/load/mysql_database_connection.py
+++ etl/load/mysql_database_connection.py
@@ -1,13 +1,32 @@
 import pymysql
 from pymysql.constants import CLIENT
 
-class SQL_database:
-    def __init__(self, host = "localhost",port = 33066,user = "root", password = "password",db = "final_project-team1", client_flag = CLIENT.MULTI_STATEMENTS, autocommit = True):
+class MySQL_Server:
+    def __init__(self, host = "localhost",port = 33066,user = "root", password = "password", client_flag = CLIENT.MULTI_STATEMENTS, autocommit = True):
         self.host = host
         self.port = port
         self.user = user
         self.password = password
-        self.db = db
+        self.autocommit = autocommit
+        self.client_flag = client_flag
+
+    def make_connection(self):
+        return pymysql.connect(
+            host = self.host,
+            port = self.port,
+            user = self.user,
+            password = self.password,
+            autocommit = self.autocommit,
+            client_flag = self.client_flag
+            )
+
+class MYSQL_database:
+    def __init__(self, database_name, host = "localhost",port = 33066,user = "root", password = "password", client_flag = CLIENT.MULTI_STATEMENTS, autocommit = True):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.db = database_name
         self.autocommit = autocommit
         self.client_flag = client_flag
 
