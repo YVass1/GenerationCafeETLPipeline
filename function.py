bd832c50cc9a0753a2bb759d0bcb8fa7dbf89453
Victor Agali <V.agali20@outlook.com>
2020-10-26 15:14:13 +0000

    New test function files

---
+++ function.py
@@ -0,0 +1,10 @@
+raw_data = {"datetime": ["11102020081100", "11102020081100"], 
+"location": ["Aberdeen","Aberdeen"], 
+"customer_name": ["Joan Pickles", "Ross Waits"], 
+"purchase": ["Large Flavoured latte - Gingerbread - 2.85, Large Flavoured latte - Vanilla - 2.85, Large Latte - 2.45, Large Flavoured latte - Gingerbread - 2.85,  Speciality Tea - Camomile - 1.30, Regular Filter coffee - 1.50"], 
+"total_price": ["11.00, 2.80"], 
+"payment_method": ["CARD", "CASH"], 
+"card_number": ["5359353452578230", "None"]}
+
+def remove_data():
+    card_number = float
