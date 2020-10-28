f17d2f279b9ae965022947619ca3c990c5ff77ef
yuhashmi <giogiosef97@gmail.com>
2020-10-28 13:16:36 +0000

    drink info list test updated

--- etl/transform/Test_drink_info_list.py
+++ etl/transform/Test_drink_info_list.py
@@ -14,8 +14,8 @@ class Test_make_drink_info_list(unittest.TestCase):
     @patch("transform_purchase.make_drink_info_list")
     def test_drink_list_returns_seperated(self, mock_make_drink_info_list):
         # Arrange
-        mock_split_info_list = []
-        expect = []
+        mock_split_info_list = ["Large Flavoured latte - Gingerbread - 2.85", "Large Flavoured latte - Vanilla - 2.85", "Large Flavoured latte - Gingerbread - 2.85"]
+        expect = [['Large Flavoured latte', 'Gingerbread', '2.85'], ['Large Flavoured latte', 'Vanilla', '2.85'], ['Large Flavoured latte', 'Gingerbread', '2.85']]
 
         # Act
         actual = make_drink_info_list(mock_split_info_list)
