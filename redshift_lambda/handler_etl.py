472d36c5caa0959508a80e80d6812a0d68da1ab8
Skyeward <mattyarnold@hotmail.co.uk>
2020-10-30 10:05:28 +0000

    removed a finally connection close block; lots of new logging

--- redshift_lambda/handler_etl.py
+++ redshift_lambda/handler_etl.py
@@ -424,6 +424,7 @@ def card_num_format(card_num_list):
 
     return starred_numbers
 
+
 ################## LOAD SECTION ################
 
 def create_database_tables(sql_code_string, connection):
@@ -449,8 +450,7 @@ def create_database_tables(sql_code_string, connection):
     except Exception as e:
         print("Executing empty string")
         print(f"Exception Error: {e}")
-    finally:
-        connection.close()
+
 
 #Function to convert any Python None values in the data to NULL (SQL only recognises NULL)
 def convert_none_data_to_null(data):
@@ -471,6 +471,7 @@ def convert_iterable_to_list_with_nones(iterable):
 
     return iterable_type(list_with_nulls)
     
+
 #Function checks if the tuple's chosen index has a None value in there or not    
 def is_value_none(index, tuple_data):
     """Checks if in data (tuple) the specified index has a None value.
@@ -480,9 +481,11 @@ def is_value_none(index, tuple_data):
         is_none = True
     return is_none
 
+
 #Function which given a datetime string, finds the corresponding day, month and year. 
 #Returns out only all the unique days, months and years found from datetimes passed in
 def corresponding_unique_days_months_years(datetimes):
+    print("corresponding_unique_days_months_years")
     """Returns lists of unique days, months and years found from datetimes passed in"""
     datetime_objects = [datetime.datetime.strptime(dtime, '%Y-%m-%d %H:%M:%S') for dtime in datetimes]
     
@@ -494,8 +497,10 @@ def corresponding_unique_days_months_years(datetimes):
     unique_years = list(set(years))
     return days, unique_days, months, unique_months, years, unique_years
 
+
 #Function to reformat data from dictionary for data to be suitable for MySQL statements 
 def reformatting_data_for_sql(data):
+    print("reformatting_data_for_sql")
     """Extracts from input dictionary and reformats relevant data into a required format for MySQL statements. """
     
     first_names  = data["fname"]
@@ -524,26 +529,28 @@ def reformatting_data_for_sql(data):
 
     return datetimes, customer_names, unique_locations, days, unique_days, months, unique_months, years,unique_years, total_prices, payment_methods, card_numbers, unique_items, all_purchases, all_items
 
+
 def insert_data_into_tables(data, connection):
+    print("insert_data_into_tables")
     """Inserts data into various database tables"""
     try:
         with connection.cursor() as cursor:
-
+            print("got cursor")
             #reformatted data suitable for MySQL statements
             datetimes, customer_names, unique_locations, days, unique_days,months, unique_months, years, unique_years, total_prices, payment_methods,card_numbers, unique_items, all_purchases, all_items = reformatting_data_for_sql(data)
 
             #insert data into tables in correct order due to dependencies (tier1 --> tier2 --> tier3)
 
             #tier 1
-
+            print("inserting data into customer table")
             #inserting data into customer table
             sql_command_insert_data_into_table = 'INSERT INTO Customers (Forename, Surname) VALUES (%s, %s)'
             cursor.executemany(sql_command_insert_data_into_table, customer_names)
-            
+            print("inserting data cafe location table")
             #inserting data into cafes locations table
             sql_command_insert_data_into_table = 'INSERT INTO Cafe_locations (Location_name) VALUES (%s)'
             cursor.executemany(sql_command_insert_data_into_table, unique_locations)
-
+            print("inserting data day;month;year tables")
             #inserting data into day, month, year tables
             sql_command_insert_data_into_table = "INSERT INTO Day (Day) VALUES (%s);"
             cursor.executemany( sql_command_insert_data_into_table, unique_days)
@@ -556,18 +563,23 @@ def insert_data_into_tables(data, connection):
 
             #inserting data into payments table
             #selecting corresponding customer ids
+            print("selecting corresponding customer ids")
             cursor.execute('SELECT c.Customer_id FROM Customers AS c')
 
-            #Extracts each row's customer_id but in tuple form: (customer_id, ) 
+            #Extracts each row's customer_id but in tuple form: (customer_id, )
+            print("Extracts each row's customer_id but in tuple form: (customer_id, )") 
             rows_of_customer_ids_tuples = cursor.FETCHALL()
 
             #Extracting customer_id out of tuple for each row and appending to customer_ids
+            print("Extracting customer_id out of tuple for each row and appending to customer_ids") 
             customer_ids = [row[0] for row in rows_of_customer_ids_tuples]
 
             #reformatting data --> each customer_id with customer's payment info in a tuple
+            print("reformatting data --> each customer_id with customer's payment info in a tuple") 
             payments_info = list(zip(customer_ids, total_prices, payment_methods, card_numbers))
             
             #inserting payment info data into payments table
+            print("inserting payment info data into payments table") 
             sql_command_insert_data_into_table = """INSERT INTO Payments (Customer_id, Total_amount, Payment_type, Card_number) VALUES (%s, %s, %s,%s);"""
             cursor.executemany(sql_command_insert_data_into_table, convert_none_data_to_null(payments_info))
 
@@ -578,6 +590,7 @@ def insert_data_into_tables(data, connection):
             #Looping through days list which is 1-to-1 mapping with datetimes list
             #For datetimes corresponding day e.g. Monday, extracting its Day_id and
             #append to day_ids list
+            print("append to day_ids list")
             day_ids = []
             for day in days:
                 cursor.execute(f'SELECT d.Day_id FROM Day AS d WHERE d.Day = %s', day )
@@ -587,6 +600,7 @@ def insert_data_into_tables(data, connection):
             #Looping through months list which is 1-to-1 mapping with datetimes list
             #For datetimes corresponding month e.g. July, extracting its Month_id and
             #append to month_ids list
+            print("append to month_ids list")
             month_ids = []
             for month in months:
                 cursor.execute(f'SELECT d.Month_id FROM Month AS d WHERE d.Month = %s', month )
@@ -595,7 +609,8 @@ def insert_data_into_tables(data, connection):
             
             #Looping through years list which is 1-to-1 mapping with datetimes list
             #For datetimes corresponding day e.g. 2020, extracting its Year_id and
-            #append to years_ids list
+            #append to year_ids list
+            print("append to year_ids list")
             year_ids = []
             for year in years:
                 cursor.execute(f'SELECT d.Year_id FROM Year AS d WHERE d.Year = %s', year )
@@ -603,21 +618,25 @@ def insert_data_into_tables(data, connection):
                 year_ids.append(year_id)
         
             #Joining lists to make a list of tuples
+            print("Joining lists to make a list of tuples")
             full_datetimes_info = list(zip(datetimes, day_ids, month_ids, year_ids))
             #OUTPUT FORMAT: 
             #full_datetimes_info = [(datetime_1, day_id, month_id, year_id),
             #(datetime_2, day_id, month_id, year_id), ...]
 
             #List of only unique datetimes_info
+            print("List of only unique datetimes_info")
             unique_datetimes =  list(set(full_datetimes_info))
 
             #inserting unique datetimes_info into table Time
+            print("inserting unique datetimes_info into table Time")
             sql_command_insert_data_into_table = """INSERT INTO Time (datetime,Day_id,Month_id,Year_id) VALUES (STR_TO_DATE(%s, "%%Y-%%m-%%d %%H:%%i:%%S"), %s,%s,%s)"""
             cursor.executemany(sql_command_insert_data_into_table, unique_datetimes)
 
             #Items table
 
             #Inserting data into Items table
+            print("inserting data into Items table")
             sql_command_insert_data_into_table = """INSERT INTO Items (Location_name, Price , Drink_type , Drink_flavour, Drink_size) VALUES (%s, %s, %s,%s,%s)"""
             cursor.executemany(sql_command_insert_data_into_table, convert_none_data_to_null(unique_items))
             
@@ -627,6 +646,7 @@ def insert_data_into_tables(data, connection):
 
             #Looping through datetimes, which have 1-to-1 relationship with customer_ids/payment_ids
             #Selecting Time_id and appending to time_ids list
+            print("Selecting Time_id and appending to time_ids list")
             time_ids = []
             for time in datetimes:
                cursor.execute("""SELECT t.Time_id From Time AS t WHERE t.datetime = %s""", time)
@@ -635,6 +655,7 @@ def insert_data_into_tables(data, connection):
 
             #Looping through customer_names list
             #Selecting payment_id connected to a customer_id which is connected to customer name
+            print("Selecting payment_id connected to a customer_id which is connected to customer name")
             payment_ids = []
             for name in customer_names:
                 cursor.execute("""select p.Payment_id from Payments AS p join Customers AS c on c.Customer_id = p.Customer_id where c.forename = %s and c.surname = %s; """, name)
@@ -644,6 +665,8 @@ def insert_data_into_tables(data, connection):
             all_purchases_with_id = list(zip(payment_ids, convert_none_data_to_null(all_purchases), time_ids))
             
             #Selecting items_id for the corresponding payment_id
+
+            print("Selecting items_id for the corresponding payment_id")
             item_ids = []
             orders_info = []
 
@@ -710,6 +733,7 @@ def insert_data_into_tables(data, connection):
                         
                         orders_info.append((payment_id,item_id,time_id))
             
+            print("executing many")
             sql_command_insert_data_into_table = """INSERT INTO Orders (Payment_id, Item_id, Time_id) VALUES (%s, %s, %s)"""           
             cursor.executemany(sql_command_insert_data_into_table, orders_info)
     # except Exception as e:
@@ -718,4 +742,3 @@ def insert_data_into_tables(data, connection):
     #     print(f"Exception Error: {e}")
     finally:
         connection.close()
-
