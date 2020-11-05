6a195adc1c051c8f3c8f692d144e3a19a49ce4be
Skyeward <mattyarnold@hotmail.co.uk>
2020-11-05 11:44:22 +0000

    corrected name of sql code file in bucket

--- lambdas/handler_load.py
+++ lambdas/handler_load.py
@@ -13,7 +13,7 @@ def start(event, context):
     print("Team One Pipeline")
     
     BUCKET_NAME = "cafe-data-data-pump-dev-team-1"
-    SQL_TEXTFILE_KEY_NAME = "create_tables_postgresql.txt"
+    SQL_TEXTFILE_KEY_NAME = "create_table_sql_code.txt"
 
     sql_code = read_from_s3(BUCKET_NAME, SQL_TEXTFILE_KEY_NAME)
 
